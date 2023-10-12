c90f3c3fa57f14919cf5f6e8d96da56b89c0caac
Fnau99 <nafikri.generali@gmail.com>
2023-10-12 19:20:30 +0200

    add file Auto.cs

---
+++ test-app/test-app/Auto.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_app
+{
+    public class Auto
+    {
+        #region tipi
+        
+        //tipi personalizzati
+        public enum motori
+        {
+            benzina, // valorizza a 0
+            diesel, // valorizzato a 1
+            elettrico // valorizzato a 2
+        }
+
+        #endregion
+
+        #region proprietà
+
+        //proprietà
+
+        private int livellocarburante;
+
+        /// <summary>
+        /// caratteristiche del veicolo
+        /// </summary>
+        public string marca;
+        public string modello;
+        public string colore;
+        public motori tipoMotore;
+        private bool accesa;
+
+        #endregion
+
+        //costruttore per inizializzare le variabili
+        public Auto()
+        {
+            this.marca = "fiat";
+            this.modello = "punto";
+            this.colore = "bianco";
+            this.tipoMotore = motori.diesel;
+        }
+
+        //preparo la stringa che contiene le proprietà dell'auto
+        public string Generadescrizione()
+        {
+
+            string result;
+
+            result = "marca: " + this.marca + "\n";
+            result += "modello: " + this.modello + "\n";
+            result += "tipo di motore: " + this.tipoMotore + "\n";
+            result += "colore: " + this.colore;
+
+            return result;
+        }
+
+        public void stampastato()
+        {
+            Console.WriteLine(this.generastato());
+        }
+        /// <summary>
+        /// genera lo stato (carburante, accensione, ...) 
+        /// </summary>
+        /// <returns></returns>
+        public string generastato()
+        {
+            string result;
+
+            result = $"carburante: {this.livellocarburante} \n";
+            if (this.accesa)
+            {
+                //se il veicolo è acceso, scrivo "accesa"
+                result += "accesa \n";
+            }
+            else
+            {
+                //altrimenti scrivo "spenta"
+                result += "spenta \n";
+            }
+
+            return result;
+        }
+
+        //output della classe auto
+        public void stampadescizione()
+        {
+            //stampo le caratteristiche dell'auto
+            Console.WriteLine(Generadescrizione());
+        }
+
+        /// <summary>
+        /// costruttore con i paramentri.
+        /// </summary>
+        /// <param name="marca">marca del veicolo</param>
+        /// <param name="modello">modello del veicolo</param>
+        /// <param name="colore">colore del veicolo</param>
+        /// <param name="tipoMotore">tipo di motore del veicolo</param>
+
+        public Auto(string marca, string modello, string colore, motori tipoMotore)
+        {
+            this.marca = marca;
+            this.modello = modello;
+            this.colore = colore;
+            this.tipoMotore = tipoMotore;
+
+        }
+
+        /// <summary>
+        /// tenta di avviare il veicolo <para>
+        /// Se non c'e carburante l'accensione fallisce</para>
+        /// </summary>
+        public void accendi()
+        {
+            //verifico il livello del carburante
+            if (this.livellocarburante > 0)
+            {
+                //Se è maggiore di 0 accendo il veicolo
+                this.accesa = true;
+            }
+            else
+            {
+                //Se è minore o uguale di zero spengo il veicolo
+                this.accesa = false;
+            }
+
+
+        }
+
+        /// <summary>
+        /// spegne il veicolo
+        /// </summary>
+        public void spegni()
+        {
+            //spegne il veicolo
+            this.accesa = false;
+        }
+
+    }
+}
--- test-app/test-app/Program.cs
+++ test-app/test-app/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Drawing;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using Test_app;
+
+Auto miaAuto;
+
+//qui valorizzo le variabili tramite il costruttore con i paramentri 
+miaAuto = new Auto("ferrari", "puro sangue", "rosso", Auto.motori.benzina);
+
+miaAuto.stampadescizione();
+miaAuto.stampastato();
+
+
+miaAuto.accendi();
+
+miaAuto.stampastato();
+
+Console.ReadLine();
+
+/*Modellare un automobile con 4 proprietà:   
+ * marca
+ * modello
+ * tipo di motore (benzina, diesel, elettrico)
+ * colore
+ * All'avvio l'app deve mostrare lo stato dell'auto, stampando marca, modello, colore e tipo di motore.
+*/
+
+//devo modellare l'automobile impletando una classe chiamata auto
+
+    
+
+   
